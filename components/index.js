29e0591b77c5794f6aeae2e1d6b4a9dea63823a3
David C <david@mokahaiku.com>
2018-04-28 19:07:24 +0100

    Add component index

---
+++ components/index.js
@@ -0,0 +1,4 @@
+export { default as Newsletter } from './Newsletter';
+export { default as HeroCarousel } from './HeroCarousel';
+export { default as Navbar } from './Navbar';
+export { default as Footer } from './Footer';
