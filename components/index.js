014bffa2a3f160916a19715b37a7b295681909a5
David C <david@mokahaiku.com>
2018-04-30 13:28:34 +0100

    Add Title and Block components to index

--- components/index.js
+++ components/index.js
@@ -2,3 +2,5 @@ export { default as Newsletter } from './Newsletter';
 export { default as HeroCarousel } from './HeroCarousel';
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
+export { default as Block } from './Block';
+export { default as Title } from './Title';
