908e1876057e7eea3fa10eab8de6136728cbb26e
David C <david@mokahaiku.com>
2018-05-01 13:10:21 +0100

    Add PlanCard component

---
+++ components/PlanCard.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from 'material-ui';
+import Typography from 'material-ui/Typography';
+import { IconCard } from './';
+
+const styles = () => ({
+  container: {
+    flex: 1,
+  },
+  price: {
+    textAlign: 'center',
+  },
+});
+
+export function PlanCard({ title, icon, price, classes, children }) {
+  return (
+    <IconCard title={title} icon={icon}>
+      <hr />
+      <Typography variant="subheading" className={classes.price}>
+        {price ? `$ ${price} / month` : 'FREE'}
+      </Typography>
+      <hr />
+      {children}
+    </IconCard>
+  );
+}
+
+PlanCard.propTypes = {
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.node.isRequired,
+  price: PropTypes.number.isRequired,
+  children: PropTypes.node.isRequired,
+  classes: PropTypes.shape({}).isRequired,
+};
+
+export default withStyles(styles)(PlanCard);
