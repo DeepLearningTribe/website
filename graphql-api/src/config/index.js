1ea1cad13347bc6315dfb1860f6a3b27ab482037
David C <david@mokahaiku.com>
2018-05-03 19:47:58 +0100

    Add mailchimp env variables to config

--- graphql-api/src/config/index.js
+++ graphql-api/src/config/index.js
@@ -2,6 +2,18 @@ import dotenv from 'dotenv';
 
 const isProduction = process.env.NODE_ENV === 'production';
 const env = isProduction ? process.env : dotenv.config().parsed;
-const { PORT, WEB_URI } = env;
+const {
+  PORT,
+  WEB_URI,
+  MAILCHIMP_REGION,
+  MAILCHIMP_LIST_ID,
+  MAILCHIMP_API_KEY,
+} = env;
 
-export { PORT, WEB_URI };
+export {
+  PORT,
+  WEB_URI,
+  MAILCHIMP_REGION,
+  MAILCHIMP_LIST_ID,
+  MAILCHIMP_API_KEY,
+};
