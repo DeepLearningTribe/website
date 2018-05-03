358b2989d17f3b13a8e4ed8e90fdca4c43785597
David C <david@mokahaiku.com>
2018-05-03 19:52:06 +0100

    Add signupForNewsletter mutation

--- graphql-api/src/resolvers/mutation.js
+++ graphql-api/src/resolvers/mutation.js
@@ -1,6 +1,8 @@
 import { login, signup } from '../connectors/user';
+import { signupForNewsletter } from '../connectors/newsletter';
 
 export default {
   login: (obj, args) => login(args),
   signup: (obj, args) => signup(args),
+  signupForNewsletter: (obj, args) => signupForNewsletter(args),
 };
--- graphql-api/src/schema/mutation.js
+++ graphql-api/src/schema/mutation.js
@@ -5,6 +5,7 @@ const Mutation = `
   type Mutation {
     login(email: String!, password: String!): Token
     signup(email: String!, password: String!): Token
+    signupForNewsletter(email: String!): Boolean
   }
 `;
 
